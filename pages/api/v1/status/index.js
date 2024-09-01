f4b9b8a7fd5fab3f50bcf20b1c1e336f249f3bbb
Daniel Lira <dnllira1@gmail.com>
2024-08-31 23:48:11 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ Chave: "São acima da média" });
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
+  const result = await database.query('select 1+1 as sum;');
+  console.log(result.rows)
+  response.status(200).json({ Chave: "Sentir-se confiante em suas decisões financeiras começa aqui. 🤝" });
 }
 
 export default status;
