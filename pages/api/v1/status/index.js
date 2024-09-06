f3d16b37ccd01044a54c5e1b20a693e307d2d375
Daniel.Dev <dnllira1@gmail.com>
2024-09-05 21:03:08 -0300

    add `jsconfig.js` file and updated `index.js`file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database.js"
+import database from "infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query('select 1+1 as sum;');
+  const result = await database.query('select 2+2 as usm;');
   console.log(result.rows)
   response.status(200).json({ Chave: "Sentir-se confiante em suas decisões financeiras começa aqui. 🤝" });
 }
