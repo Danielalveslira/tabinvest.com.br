90748a43384859fa64914d429fede800cd5f91e5
Daniel.Dev <dnllira1@gmail.com>
2024-09-15 20:52:15 -0300

    adds `/api/v1/migrations` endpoint

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const nextJest = require("next/jest");
+const dotenv = require("dotenv");
+
+
+dotenv.config({ path: ".env.development" });
+
+const createJestConfig = nextJest();
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  testEnvironment: "node",
+});
+
+module.exports = jestConfig;
