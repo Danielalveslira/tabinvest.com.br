1f26a382f7d483bfe20ca94da45b07bae8846c38
Daniel.Dev <dnllira1@gmail.com>
2025-04-06 12:11:41 -0300

    feat: create and use `NotFoundError` in `controller.js`

--- infra/controller.js
+++ infra/controller.js
@@ -2,8 +2,8 @@ import {
   InternalServerError,
   MethodNotAllowedError,
   ValidationError,
+  NotFoundError,
 } from "infra/errors";
-import { validate } from "uuid";
 
 function onNoMatchHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
@@ -11,7 +11,7 @@ function onNoMatchHandler(request, response) {
 }
 
 function onErrorHandler(error, request, response) {
-  if (error instanceof ValidationError) {
+  if (error instanceof ValidationError || error instanceof NotFoundError) {
     return response.status(error.statusCode).json(error);
   }
 
