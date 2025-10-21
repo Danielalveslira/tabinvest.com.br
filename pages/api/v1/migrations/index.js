d94e62aa757c41404e56b5e154493cb2c018a4fb
Wellington Lira <dnllira1@gmail.com>
2025-10-21 08:56:38 -0300

    feat: implement `/api/v1/user` endpoint with session renewal

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -7,7 +7,7 @@ const router = createRouter();
 router.get(getHandler);
 router.post(postHandler);
 
-export default router.handler(controller.errorsHandlers);
+export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
   const pendingMigrations = await migrator.listPendingMigrations();
