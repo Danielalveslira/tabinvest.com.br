935681cb37f41dc38743ddb325de6fcc84a490f4
Wellington Lira <dnllira1@gmail.com>
2026-03-15 19:02:48 -0300

    feat: add `injectAnonymousOrUser` and `canRequest` middlewares to `/sessions`

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -2,10 +2,12 @@ import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
 import authentication from "models/authentication";
 import session from "models/session.js";
+import next from "next";
 
 const router = createRouter();
 
-router.post(postHandler);
+router.use(controller.injectAnonymousOrUser);
+router.post(controller.canRequest("create:session"), postHandler);
 router.delete(deleteHandler);
 
 export default router.handler(controller.errorHandlers);
--- tests/integrations/_use-cases/registration-flow.test.js
+++ tests/integrations/_use-cases/registration-flow.test.js
@@ -84,7 +84,27 @@ describe("Use case: Registration Flow (all sucessful)", () => {
     expect(activatedUser.features).toEqual(["create:session"]);
   });
 
-  test("Login", () => {});
+  test("Login", async () => {
+    const createSessionsResponse = await fetch(
+      "http://localhost:3000/api/v1/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "registration.flow@gmail.com",
+          password: "RegistrationFlowPassword",
+        }),
+      },
+    );
+
+    expect(createSessionsResponse.status).toBe(201);
+
+    const createSessionsResponseBody = await createSessionsResponse.json();
+
+    expect(createSessionsResponseBody.user_id).toBe(createUserResponseBody.id);
+  });
 
   test("Get user information", () => {});
 });
